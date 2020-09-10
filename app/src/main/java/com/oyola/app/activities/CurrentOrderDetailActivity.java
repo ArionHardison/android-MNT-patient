aedbc8240c2b14a7fa8510531168e26d33b22e00
NandagopalR <gopal300491@gmail.com>
2020-09-10 20:52:47 +0530

    Pickup order status issue fixed

--- app/src/main/java/com/oyola/app/activities/CurrentOrderDetailActivity.java
+++ app/src/main/java/com/oyola/app/activities/CurrentOrderDetailActivity.java
@@ -332,23 +332,17 @@ public class CurrentOrderDetailActivity extends BaseActivity implements OnMapRea
         List<OrderFlow> orderFlowList = new ArrayList<>();
         if (GlobalData.isSelectedOrder != null) {
             if (GlobalData.isSelectedOrder.getPickUpRestaurant() == 0) {
-                orderFlowList.add(new OrderFlow(getString(R.string.order_placed_new), getString(R.string.description_1), R.drawable.ic_order_placed, ORDER_STATUS.get(0)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_confirmed_new), getString(R.string.description_2), R.drawable.ic_order_confirmed, ORDER_STATUS.get(1)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_processed_new), getString(R.string.description_3), R.drawable.ic_order_processed, ORDER_STATUS.get(2) + ORDER_STATUS.get(3) + ORDER_STATUS.get(4)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_pickedup_new), getString(R.string.description_4), R.drawable.ic_order_picked_up, ORDER_STATUS.get(5) + ORDER_STATUS.get(6)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_delivered_new), getString(R.string.description_5), R.drawable.ic_order_delivered, ORDER_STATUS.get(7) + ORDER_STATUS.get(10)));
-            } else if (GlobalData.isSelectedOrder.getPickUpRestaurant() == 1) {
-                orderFlowList.add(new OrderFlow(getString(R.string.order_placed_new), getString(R.string.description_1_new), R.drawable.ic_order_placed, ORDER_STATUS.get(0)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_confirmed_new), getString(R.string.description_2_new), R.drawable.ic_order_confirmed, ORDER_STATUS.get(1)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_processed_new), getString(R.string.description_3_new), R.drawable.ic_order_processed, ORDER_STATUS.get(2) + ORDER_STATUS.get(3) + ORDER_STATUS.get(4) + ORDER_STATUS.get(7) + ORDER_STATUS.get(8) + ORDER_STATUS.get(9)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_pickedup), getString(R.string.description_4_new), R.drawable.ic_order_picked_up, ORDER_STATUS.get(5) + ORDER_STATUS.get(6)));
-//                orderFlowList.add(new OrderFlow(getString(R.string.order_delivered_new), getString(R.string.description_5_new), R.drawable.ic_order_delivered, ORDER_STATUS.get(7) + ORDER_STATUS.get(10)));
+                orderFlowList.add(new OrderFlow(getString(R.string.delivery_order_received), getString(R.string.delivery_description_1), R.drawable.ic_order_placed, ORDER_STATUS.get(0)));
+                orderFlowList.add(new OrderFlow(getString(R.string.delivery_order_accepted), getString(R.string.delivery_description_2), R.drawable.ic_order_confirmed, ORDER_STATUS.get(1)));
+                orderFlowList.add(new OrderFlow(getString(R.string.delivery_order_processed_new), getString(R.string.delivery_description_3), R.drawable.ic_order_processed, ORDER_STATUS.get(2) + ORDER_STATUS.get(3) + ORDER_STATUS.get(4)));
+                orderFlowList.add(new OrderFlow(getString(R.string.delivery_order_pickedup_new), getString(R.string.delivery_description_4), R.drawable.ic_order_picked_up, ORDER_STATUS.get(5) + ORDER_STATUS.get(6)));
+                orderFlowList.add(new OrderFlow(getString(R.string.delivery_order_arrived), getString(R.string.delivery_description_5), R.drawable.ic_order_delivered, ORDER_STATUS.get(7) + ORDER_STATUS.get(10)));
             } else {
-                orderFlowList.add(new OrderFlow(getString(R.string.order_placed_new), getString(R.string.description_1_new), R.drawable.ic_order_placed, ORDER_STATUS.get(0)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_confirmed_new), getString(R.string.description_2_new), R.drawable.ic_order_confirmed, ORDER_STATUS.get(1)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_processed_new), getString(R.string.description_3_new), R.drawable.ic_order_processed, ORDER_STATUS.get(2) + ORDER_STATUS.get(3) + ORDER_STATUS.get(4)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_pickedup_new), getString(R.string.description_4_new), R.drawable.ic_order_picked_up, ORDER_STATUS.get(5) + ORDER_STATUS.get(6)));
-                orderFlowList.add(new OrderFlow(getString(R.string.order_delivered_new), getString(R.string.description_5_new), R.drawable.ic_order_delivered, ORDER_STATUS.get(7) + ORDER_STATUS.get(10)));
+                orderFlowList.add(new OrderFlow(getString(R.string.pickup_order_received), getString(R.string.pickup_description_1), R.drawable.ic_order_placed, ORDER_STATUS.get(0)));
+                orderFlowList.add(new OrderFlow(getString(R.string.pickup_order_accepted), getString(R.string.pickup_description_2), R.drawable.ic_order_confirmed, ORDER_STATUS.get(1)));
+                orderFlowList.add(new OrderFlow(getString(R.string.pickup_order_processed_new), getString(R.string.pickup_description_3), R.drawable.ic_order_processed, ORDER_STATUS.get(2) + ORDER_STATUS.get(3) + ORDER_STATUS.get(4) + ORDER_STATUS.get(7) + ORDER_STATUS.get(8) + ORDER_STATUS.get(9)));
+                orderFlowList.add(new OrderFlow(getString(R.string.pickup_order_pickedup_new), getString(R.string.pickup_description_4), R.drawable.ic_order_picked_up, ORDER_STATUS.get(5) + ORDER_STATUS.get(6)));
+                orderFlowList.add(new OrderFlow(getString(R.string.pickup_order_completed), getString(R.string.pickup_description_5), R.drawable.ic_order_delivered, ORDER_STATUS.get(7) + ORDER_STATUS.get(10)));
             }
         }
 
