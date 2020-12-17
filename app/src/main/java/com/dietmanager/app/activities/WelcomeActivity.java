b90e2032fbbe54f365a92ce4e2a3e9f7712abdeb
SHYAM SUNDAR J <shyam.j@appoets.com>
2020-12-17 20:58:37 +0530

    Bug fixes

--- app/src/main/java/com/dietmanager/app/activities/WelcomeActivity.java
+++ app/src/main/java/com/dietmanager/app/activities/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.dietmanager.app.activities;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
@@ -12,9 +13,18 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
+
+import android.text.Html;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -44,6 +54,11 @@ public class WelcomeActivity extends BaseActivity implements
     private static final int PLAY_SERVICES_REQUEST = 1000;
     private static final int REQUEST_CHECK_SETTINGS = 2000;
 
+    private MyViewPagerAdapter myViewPagerAdapter;
+    @BindView(R.id.layoutDots)
+    LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
     @BindView(R.id.vp_welcome)
     ViewPager welcomePager;
     @BindView(R.id.tl_page_indicator)
@@ -96,6 +111,7 @@ public class WelcomeActivity extends BaseActivity implements
 ////                finish();
 //            }
 //        });
+
     }
 
     @OnClick({R.id.btn_continue})
@@ -115,7 +131,28 @@ public class WelcomeActivity extends BaseActivity implements
         else
             continueButton.setText(getString(R.string.continue_txt));
     }*/
+   ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
 
+       @Override
+       public void onPageSelected(int position) {
+           addBottomDots(position);
+
+       }
+
+       @Override
+       public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+//            ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) viewPager.getLayoutParams();
+//            params.setMargins((int) ((position + positionOffset) * 500), 0, 0, 0);
+//            viewPager.setLayoutParams(params);
+
+       }
+
+       @Override
+       public void onPageScrollStateChanged(int arg0) {
+
+       }
+   };
     private void initializeViewReferences() {
         ButterKnife.bind(this);
         welcomePager = findViewById(R.id.vp_welcome);
@@ -123,11 +160,25 @@ public class WelcomeActivity extends BaseActivity implements
         pageIndicatorTab = findViewById(R.id.tl_page_indicator);
         pageIndicatorTab.setupWithViewPager(welcomePager, true);
         continueButton = findViewById(R.id.btn_continue);
+        layouts = new int[]{
+                R.layout.welcome_slide1,
+                R.layout.welcome_slide2,
+                R.layout.welcome_slide3
+        };
 
+        dotsLayout = findViewById(R.id.layoutDots);
+/*
         WelcomePagerAdapter adapter = new WelcomePagerAdapter(getBaseContext());
         welcomePager.setAdapter(adapter);
-        welcomePager.setCurrentItem(0, true);
+        welcomePager.setCurrentItem(0, true);*/
         continueButton.setText(getString(R.string.signin));
+
+
+        addBottomDots(0);
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        welcomePager.setAdapter(myViewPagerAdapter);
+        welcomePager.addOnPageChangeListener(viewPagerPageChangeListener);
     }
 
     private void setPageSelection() {
@@ -142,6 +193,29 @@ public class WelcomeActivity extends BaseActivity implements
         } else if (welcomePager.getCurrentItem() < WelcomePagerAdapter.PAGE_COUNT) {
             welcomePager.setCurrentItem(welcomePager.getCurrentItem() + 1, true);
         }
+    }
+
+
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[layouts.length];
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom_in);
+            dots[currentPage].setTextColor(colorsActive[currentPage]);
+            dots[currentPage].startAnimation(animation);
+        }
+
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -255,6 +329,38 @@ public class WelcomeActivity extends BaseActivity implements
                     }
                 }
                 break;
+        }
+    }
+
+
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
         }
     }
 }
